63c0b3e41de79519e4dfae3bae188534c9fca7fb
Kush Kumar <kushkushwaha52@gmail.com>
2024-03-10 22:32:52 +0530

    tutorial part 1

--- src/main.js
+++ src/main.js
@@ -1,27 +1,19 @@
-import { Boot } from './scenes/Boot';
 import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#1d212d',
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
     },
     scene: [
-        Boot,
-        Preloader,
-        MainMenu,
         Game,
-        GameOver
     ]
 };
 
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,24 +5,57 @@ export class Game extends Scene
     constructor ()
     {
         super('Game');
+        let shiftKey;
+        let cursors;
+        let controls;
+        let glayer;
+        let marker;
     }
 
+    preload (){
+        this.load.image('tiles', 'assets/tilesets/0x72-industrial-tileset-32px-extruded.png');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/platformer-simple.json');
+    }
     create ()
     {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('0x72-industrial-tileset-32px-extruded', 'tiles');
+        map.createLayer('Background', tileset);
+        this.glayer = map.createLayer('Ground', tileset).setCollisionByProperty({collides: true});
+        map.createLayer('Foreground', tileset);
+        this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        const controlConfig = {
+            camera: this.cameras.main,
+            left: this.cursors.left,
+            right: this.cursors.right,
+            up: this.cursors.up,
+            down: this.cursors.down,
+            speed: 0.5
+        };
+        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.marker = this.add.graphics();
+        this.marker.lineStyle(5, 0xffffff, 1);
+        this.marker.strokeRect(0, 0,map.tileWidth,map.tileHeight);
+        this.marker.lineStyle(3, 0xff4f78, 1);
+        this.marker.strokeRect(0, 0,map.tileWidth,map.tileHeight);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+    }
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
+    update(time, delta){
+        this.controls.update(delta);
+        const worldPos = this.input.activePointer.positionToCamera(this.cameras.main);
+        const pointXY = this.glayer.worldToTileXY(worldPos.x, worldPos.y);
+        const snapped = this.glayer.tileToWorldXY(pointXY.x, pointXY.y);
+        this.marker.setPosition(snapped.x, snapped.y);  
+        if (this.input.manager.activePointer.isDown){
+            if (this.shiftKey.isDown){
+                this.glayer.removeTileAtWorldXY(worldPos.x, worldPos.y);
+            }
+            else {
+                this.glayer.putTileAtWorldXY(6,worldPos.x,worldPos.y);
+            }
+        }
     }
 }
